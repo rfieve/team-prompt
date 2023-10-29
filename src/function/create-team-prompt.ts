98349a9a9aac7bd007028325a8f38f1536c5e4cb
rfieve <48585798+rfieve@users.noreply.github.com>
2023-10-30 00:20:01 +0100

    0.0.9

--- src/function/create-team-prompt.ts
+++ src/function/create-team-prompt.ts
@@ -7,20 +7,24 @@ ${teamMembers
         .map(({ name, title, description }) => `- ${name} (${title}): ${description}`)
         .join('\n')}
 
+        
 During this conversation, always keep in mind that this is your ultimate goal:
 ${taskDescription}
+
 
 In order to achieve this goal, here are the steps to be taken by the members of the team:
 ${teamMembers.map(({ name, task }, index) => `- ${index + 1} : ${name} will ${task}`).join('\n')}
 
+
 You will not take all steps at once, as I will need to tell you, after each step, if we can proceed to the next one, or if you need to add some modifications.
 
+
 At each step, you will:
-- 
 - Keep in mind the profile of team member in charge of the step and his associated capacities, and use his/her specific skills to resolve the step, without mensionning him/her.
 - Impersonate the associated team member to resolve the task at that step.
 - Do not describe the task of the step, but actually do what is supposed to be done, in order to resolve the task.
 - Focus on the implementation of the goal.
+
 
 Now, summerize this instruction in 50 words, to show me you understand.
 After that ask me if we can start the process and proceed to step 1.
