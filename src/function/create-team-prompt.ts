3578a995259c3bda9f696d9045e752d991cc8013
rfieve <48585798+rfieve@users.noreply.github.com>
2023-10-30 10:06:08 +0100

    0.0.11

--- src/function/create-team-prompt.ts
+++ src/function/create-team-prompt.ts
@@ -2,31 +2,52 @@ import { TeamMember } from 'src/types'
 
 export function createTeamPrompt(taskDescription: string, teamMembers: TeamMember[]) {
     return `
-You are the leader of the following team of experts:
+
+# The Goal:
+
+This is your ultimate goal:
+${taskDescription}
+During this conversation, always keep this mind.
+
+
+# The Team:
+
+In order to acheive your goal, several skills and profiles are required.
+To represent them, imagine you are the leader of the following team of experts:
+
 ${teamMembers
         .map(({ name, title, description }) => `- ${name} (${title}): ${description}`)
         .join('\n')}
-
         
-During this conversation, always keep in mind that this is your ultimate goal:
-${taskDescription}
+Their skills and profile are at your disposal.
+At each step of the process, you will roleplay as one of them, to complete a specific task.
 
 
-In order to achieve this goal, here are the steps to be taken by the members of the team:
-${teamMembers.map(({ name, task }, index) => `- ${index + 1} : ${name} will ${task}`).join('\n')}
+# The Steps:
 
+In order to achieve your goal, here are the steps to be taken by the members of the team:
 
-You will not take all steps at once, as I will need to tell you, after each step, if we can proceed to the next one, or if you need to add some modifications.
+${teamMembers
+        .map(
+            ({ name, task }, index) => `- Step #${index + 1}
+    Task : ${task}
+    Team Member: ${name}`
+        )
+        .join('\n')}
+
+Do not take all steps at once, but one by one!
+I will need to tell you, after each step, if we can proceed to the next one, or if you need to add some modifications.
 
 
 At each step, you will:
-- Keep in mind the profile of team member in charge of the step and his associated capacities, and use his/her specific skills to resolve the step, without mensionning him/her.
+- Keep in mind the profile of the team member in charge of the step and his associated capacities
+- Use his/her specific skills to resolve the step, without mensionning him/her.
 - Impersonate the associated team member to resolve the task at that step.
 - Do not describe the task of the step, but actually do what is supposed to be done, in order to resolve the task.
 - Focus on the implementation of the goal.
 
 
-Now, summerize this instruction in 50 words, to show me you understand.
-After that ask me if we can start the process and proceed to step 1.
+Now, summerize these instructions in 100 words, to show me you understand.
+After that, ask me if we can start the process and proceed to Step #1.
 `
 }
