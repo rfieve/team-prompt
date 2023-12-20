60209181ac5febc64798a02dcfcac8c1532e0952
rfieve <48585798+rfieve@users.noreply.github.com>
2023-12-20 19:40:10 +0100

    0.0.37

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 export { teamMembers } from './constants/team-members'
 export { createTeamPrompt } from './function/create-team-prompt'
 export { buildTeamMember } from './function/build-team-member'
-export { TeamMember, TeamMemberTag, Step } from './types'
+export {
+    TeamMember,
+    TeamMemberTag,
+    TeamMemberOptionType,
+    TeamMemberOptionString,
+    TeamMemberOptionNumber,
+    TeamMemberOption,
+    Step,
+} from './types'
